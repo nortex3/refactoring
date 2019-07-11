4b6faa75299ee813644c9a4d4ac9915eb1bdf785
Hugo Meireles <hugo.meireles@novabase.com>
2019-07-11 01:26:51 +0100

    change name get to find in Operations interface

--- src/main/java/com/celfocus/training/business/Operations.java
+++ src/main/java/com/celfocus/training/business/Operations.java
@@ -6,7 +6,7 @@ import com.celfocus.training.business.exception.SaveException;
 public interface Operations<T> {
 
     void save(T t) throws SaveException;
-    T get(String name);
+    T find(String name);
     boolean delete(T t) throws DeleteException;
 
 }
