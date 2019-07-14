36703099fe2be80720bf157c9aff16d94a1a59ec
Hugo Meireles <hugo.meireles@novabase.com>
2019-07-14 19:28:39 +0100

    remove product from discount model

--- src/main/java/com/celfocus/training/model/Discount.java
+++ src/main/java/com/celfocus/training/model/Discount.java
@@ -2,17 +2,11 @@ package com.celfocus.training.model;
 
 public class Discount extends AbstractModel {
 
-    private Product product;
     private double discount;
 
-    public Discount(int id, Product product, double discount) {
+    public Discount(int id, double discount) {
         super(id);
-        this.product = product;
         this.discount = discount;
-    }
-
-    public Product getProduct() {
-        return product;
     }
 
     public double getDiscount() {
